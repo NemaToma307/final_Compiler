e72a0cc0a4ce57e85f523b4cb50240c413e5ef51
NemaToma307 <nany.toma.nt@gmail.com>
2023-01-03 00:34:47 +0300

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,7 @@ public class Main {
             System.out.println("hiiii");
             System.out.println("hii");
             System.out.println("hii");
+            System.out.println("hii");
 
 
         } catch (IOException e) {
